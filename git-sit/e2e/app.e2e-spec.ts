24d00a919fc728de22836095fd2c29f7a83cb3dc
KANG BIN KIM <kimkb2011@gmail.com>
2018-04-08 15:41:10 -0400

    2nd commit

---
+++ git-sit/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('git-sit App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
