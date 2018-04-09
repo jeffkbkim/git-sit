6aa908bec24f875ab86043dbda3cdfd247899130
KANG BIN KIM <kimkb2011@gmail.com>
2018-04-08 22:21:03 -0400

    prototype completed. need website to embed

--- git-sit/src/app/app-routing.module.ts
+++ git-sit/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainpageComponent } from './mainpage/mainpage.component';
-import { TeamComponent } from './team/team.component';
 import { FeaturesComponent } from './features/features.component';
 
 const routes: Routes = [
@@ -9,10 +8,6 @@ const routes: Routes = [
     path: '',
     component: MainpageComponent,
     pathMatch: 'full'
-  },
-  {
-    path: 'team',
-    component: TeamComponent
   },
   {
     path: 'features',
--- git-sit/src/app/app.module.ts
+++ git-sit/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { MainpageComponent } from './mainpage/mainpage.component';
-import { TeamComponent } from './team/team.component';
 import { FeaturesComponent } from './features/features.component';
 
 
@@ -14,7 +13,6 @@ import { FeaturesComponent } from './features/features.component';
   declarations: [
     AppComponent,
     MainpageComponent,
-    TeamComponent,
     FeaturesComponent
   ],
   imports: [
